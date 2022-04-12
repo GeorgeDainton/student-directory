c9371ffcb8c9b156756a74330a17abdf225fddcc
George Dainton <gcdainton94@gmail.com>
2022-04-12 13:00:59 +0100

    Added country of birth catagory to inout_students and print students method.

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -5,14 +5,23 @@ class String
 end
 
 def input_students
-  puts "Please enter the names of the students"
+  puts "Enter student:"
   puts "To finish, just hit return twice"
   students = []
   name = gets.chomp.titleize
-  while !name.empty? do
-    students << {name: name, cohort: :november}
+  puts "What country were they born in?:"
+  country = gets.chomp.titleize
+  while !name.empty? || !country.empty? do
+    students << {name: name, country: country, cohort: :november}
     puts "Now we have #{students.count} students"
+    puts "Enter student:"
     name = gets.chomp.titleize
+      if name.empty?
+        break
+      else
+    puts "What country were they born in?:"
+      end
+    country = gets.chomp.titleize
   end
   students 
 end 
@@ -23,10 +32,8 @@ def print_header
 end 
 
 def print(students)
-  students.each do |student|
-    if student[:name].length < 12
-      puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    end
+  students.each.with_index(1) do |student, index|
+    puts "#{index}. #{student[:name]}, born in #{student[:country]}, (#{student[:cohort].capitalize} cohort)"
   end
 end
 
