94d3c7d4b78377c5c0b1978b55b01d68ffd45d53
George Dainton <gcdainton94@gmail.com>
2022-04-12 14:19:18 +0100

    Added height catagory to directory and wrote nested while loop in input_students to fix issue with hitting return twice not ending the input

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -5,43 +5,18 @@ class String
 end
 
 def input_students
-  puts "Enter student:"
-  puts "To finish, just hit return twice"
   students = []
-  name = gets.chomp.titleize
-  puts "What country were they born in?:"
-  country = gets.chomp.titleize
-  while !name.empty? || !country.empty? do
+  puts "To finish, just hit return twice"
+  while true do
     students << {name: name, country: country, cohort: :november}
-    puts "Now we have #{students.count} students"
     puts "Enter student:"
     name = gets.chomp.titleize
-      if name.empty?
-        break
-      else
-    puts "What country were they born in?:"
+      while !name.empty? do
+        puts "What country were they born in?:"
+        country = gets.chomp.titleize
       end
-    country = gets.chomp.titleize
-  end
-  students 
-end 
-
-def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
-end 
-
-def print(students)
-  students.each.with_index(1) do |student, index|
-    puts "#{index}. #{student[:name]}, born in #{student[:country]}, (#{student[:cohort].capitalize} cohort)"
-  end
+    end
+  students
 end
 
-def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
-end
-
-students = input_students
-print_header
-print(students)
-print_footer(students)
+students = input_students    
