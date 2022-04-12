66810042c5506d636aec2ea502b6c17b36fb1998
George Dainton <gcdainton94@gmail.com>
2022-04-12 14:25:58 +0100

    Modified cohort so it is now user generated and changed to a symbol once input

--- directory_exercises.rb
+++ directory_exercises.rb
@@ -4,19 +4,46 @@ class String
   end
 end
 
+
 def input_students
   students = []
   puts "To finish, just hit return twice"
   while true do
-    students << {name: name, country: country, cohort: :november}
     puts "Enter student:"
     name = gets.chomp.titleize
+    if name == ""
+      break
+    end
       while !name.empty? do
         puts "What country were they born in?:"
-        country = gets.chomp.titleize
+          country = gets.chomp.titleize
+        puts "How tall are they (cm)?:"
+          height = gets.chomp.titleize
+        puts "What cohort are they in?:"
+          cohort = gets.chomp.to_sym
+        break
       end
-    end
+    students << {name: name, country: country, height: height, cohort: cohort}
+  end
   students
 end
+     
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end 
 
-students = input_students    
+def print(students)
+  students.each.with_index(1) do |student, index|
+    puts "#{index}. #{student[:name]}, born in #{student[:country]}, #{student[:height]} cm (#{student[:cohort].capitalize} cohort)"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
